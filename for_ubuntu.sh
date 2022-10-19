7e8f3d26705d0520283795417e66af12a879b9d3
Agung Raka Wibawa <77326619+git-raka@users.noreply.github.com>
2022-10-19 10:10:21 +0700

    Update for_ubuntu.sh

--- for_ubuntu.sh
+++ for_ubuntu.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "https://github.com/git-raka"
 sudo apt install openjdk-11-jre-headless -y
-wget https://neo4j.com/artifact.php?name=neo4j-enterprise-4.4.10-unix.tar.gz -q --show-progress
+wget https://neo4j.com/artifact.php?name=neo4j-enterprise-4.4.12-unix.tar.gz -q --show-progress
 
-cp 'artifact.php?name=neo4j-enterprise-4.4.10-unix.tar.gz'  neo4j-enterprise-4.4.10-unix.tar.gz && rm 'artifact.php?name=neo4j-enterprise-4.4.10-unix.tar.gz'
-tar -xvf neo4j-enterprise-4.4.10-unix.tar.gz
+cp 'artifact.php?name=neo4j-enterprise-4.4.12-unix.tar.gz'  neo4j-enterprise-4.4.12-unix.tar.gz && rm 'artifact.php?name=neo4j-enterprise-4.4.12-unix.tar.gz'
+tar -xvf neo4j-enterprise-4.4.12-unix.tar.gz
 
 var=$(pwd)
 echo "The current working directory $var"
 
 #define filename
-cd $var/neo4j-enterprise-4.4.10/conf
+cd $var/neo4j-enterprise-4.4.12/conf
 filename='neo4j.conf'
 
 # Define ip
@@ -22,21 +22,21 @@ echo "dbms.security.procedures.unrestricted=apoc.*,gds.*,bloom.*
 dbms.security.procedures.allowlist=apoc.*,gds.*,bloom.*
 apoc.import.file.enabled=true
 apoc.import.file.use_neo4j_config=true
-neo4j.bloom.license_file=/home/mri/neo4j-enterprise-4.4.10/licenses/bloom.txt
-gds.enterprise.license_file=/home/mri/neo4j-enterprise-4.4.10/licenses/gds.txt
+neo4j.bloom.license_file=/home/mri/neo4j-enterprise-4.4.12/licenses/bloom.txt
+gds.enterprise.license_file=/home/mri/neo4j-enterprise-4.4.12/licenses/gds.txt
 dbms.unmanaged_extension_classes=com.neo4j.bloom.server=/bloom
 dbms.security.http_auth_allowlist=/,/browser.*,/bloom.*
 dbms.default_listen_address=0.0.0.0
 dbms.default_advertised_address=$ip" >> $filename
 
 # add plugin
-cd $var/neo4j-enterprise-4.4.10/plugins
+cd $var/neo4j-enterprise-4.4.12/plugins
 wget https://github.com/neo4j/graph-data-science/releases/download/2.1.8/neo4j-graph-data-science-2.1.8.jar
 wget https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.4.0.8/apoc-4.4.0.8-all.jar
 
 # start neo4j
 
-cd $var/neo4j-enterprise-4.4.10/bin/
+cd $var/neo4j-enterprise-4.4.12/bin/
 ./neo4j start
 
 echo "Neo4j Sucessfuly installed,open your browser in $ip:7474"
