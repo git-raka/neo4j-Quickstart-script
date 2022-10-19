276da46bddda362b5309266356c7fe9e02919b33
Agung Raka Wibawa <77326619+git-raka@users.noreply.github.com>
2022-10-19 10:03:19 +0700

    Update for_centos7.sh

--- for_centos7.sh
+++ for_centos7.sh
@@ -2,16 +2,16 @@
 echo "https://github.com/git-raka"
 yum install java-11-openjdk-devel -y 
 yum install wget -y
-wget https://neo4j.com/artifact.php?name=neo4j-enterprise-4.4.10-unix.tar.gz
+wget https://neo4j.com/artifact.php?name=neo4j-enterprise-4.4.12-unix.tar.gz
 
-cp 'artifact.php?name=neo4j-enterprise-4.4.10-unix.tar.gz'  neo4j-enterprise-4.4.10-unix.tar.gz && rm 'artifact.php?name=neo4j-enterprise-4.4.10-unix.tar.gz'
-tar -xvf neo4j-enterprise-4.4.10-unix.tar.gz
+cp 'artifact.php?name=neo4j-enterprise-4.4.12-unix.tar.gz'  neo4j-enterprise-4.4.12-unix.tar.gz && rm 'artifact.php?name=neo4j-enterprise-4.4.12-unix.tar.gz'
+tar -xvf neo4j-enterprise-4.4.12-unix.tar.gz
 
 var=$(pwd)
 echo "The current working directory $var"
 
 #define filename
-cd $var/neo4j-enterprise-4.4.10/conf
+cd $var/neo4j-enterprise-4.4.12/conf
 filename='neo4j.conf'
 
 # Define ip
@@ -27,14 +27,14 @@ dbms.default_listen_address=0.0.0.0
 dbms.default_advertised_address=$ip" >> $filename
 
 # add plugin
-cd $var/neo4j-enterprise-4.4.10/plugins
+cd $var/neo4j-enterprise-4.4.12/plugins
 wget https://github.com/neo4j/graph-data-science/releases/download/2.1.8/neo4j-graph-data-science-2.1.8.jar
 wget https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.4.0.8/apoc-4.4.0.8-all.jar
 
 # start neo4j
 
-cd $var/neo4j-enterprise-4.4.10/bin/
+cd $var/neo4j-enterprise-4.4.12/bin/
 ./neo4j start
-rm -rf neo4j-enterprise-4.4.10-unix.tar.gz
+rm -rf neo4j-enterprise-4.4.12-unix.tar.gz
 
 echo "Neo4j Sucessfuly installed,open your browser in $ip:7474"
